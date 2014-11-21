cbfe115dec9649dadce33286a2de0cc07a5d0ef7
Carsten Zimmermann <cz@aegisnet.de>
2014-11-21 16:51:24 +0100

    Automatically add middleware to host app

--- lib/schneiderlein/engine.rb
+++ lib/schneiderlein/engine.rb
@@ -1,5 +1,11 @@
 module Schneiderlein
   class Engine < ::Rails::Engine
     isolate_namespace Schneiderlein
+
+    initializer 'schneiderlein.middleware' do |app|
+      app.config.middleware.insert_before \
+        'ActionDispatch::ParamsParser', 'Schneiderlein::FlyCatcher'
+    end
   end
+
 end
