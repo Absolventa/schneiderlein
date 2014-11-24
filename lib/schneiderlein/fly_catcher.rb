2bb352d63d27755c59ba16a51e1667cf34d82265
Robin Neumann <robin.neumann@absolventa.de>
2014-11-24 16:55:52 +0100

    Encapsulate resetting env variables

--- lib/schneiderlein/fly_catcher.rb
+++ lib/schneiderlein/fly_catcher.rb
@@ -5,14 +5,19 @@ module Schneiderlein
         app.call(env)
       rescue ActionDispatch::ParamsParser::ParseError => e
         env['rack.schneiderlein.parse_errors'] = Array(e.message)
-
-        env['rack.input']     = StringIO.new
-        env['rack.errors']    = StringIO.new
-        env['RAW_POST_DATA']  = ''
-        env['CONTENT_LENGTH'] = '0'
-
-        app.call(env)
+        app.call(remove_errors_from(env))
       end
     end
+
+    private
+
+    def remove_errors_from(env)
+      env['rack.input']     = StringIO.new
+      env['rack.errors']    = StringIO.new
+      env['RAW_POST_DATA']  = ''
+      env['CONTENT_LENGTH'] = '0'
+      env
+    end
+
   end
 end
