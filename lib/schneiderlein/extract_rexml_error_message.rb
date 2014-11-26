57a80fe13f85b76d7a79982250c7abd2699dc5b4
Robin Neumann <robin.neumann@absolventa.de>
2014-11-26 13:45:43 +0100

    Add Error message extractor class

---
+++ lib/schneiderlein/extract_rexml_error_message.rb
@@ -0,0 +1,15 @@
+module Schneiderlein
+  class ExtractREXMLErrorMessage < Struct.new(:exception)
+
+    def to_s
+      sanitizer(exception.message)
+    end
+
+    private
+
+    def sanitizer(str)
+      str.to_s.gsub!('#<REXML::ParseException: ', '').to_s[0..str.to_s.index('Last').to_i-2]
+    end
+
+  end
+end
