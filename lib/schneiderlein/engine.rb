0985668abbea9004c2d0a992a5f9bb425f4a63ea
Carsten Zimmermann <cz@aegisnet.de>
2016-02-22 18:19:23 +0100

    Use a different fixpoint to insert Schneiderlein
    ActionDispatch::Request is gone in Rails 5. By default, ActionDispatch::Request was inserted
    after ActionDispatch::Callbacks so this is where we put ourselves.

--- lib/schneiderlein/engine.rb
+++ lib/schneiderlein/engine.rb
@@ -3,8 +3,15 @@ module Schneiderlein
     isolate_namespace Schneiderlein
 
     initializer 'schneiderlein.middleware' do |app|
-      app.config.middleware.insert_before \
-        'ActionDispatch::ParamsParser', 'Schneiderlein::FlyCatcher'
+      if Rails::VERSION::MAJOR < 5
+        app.config.middleware.insert_before \
+          'ActionDispatch::ParamsParser', 'Schneiderlein::FlyCatcher'
+      else
+        # ActionDispatch::ParamsParser is not included in the middleware
+        # stack anymore in Rails 5.
+        app.config.middleware.insert_after \
+          'ActionDispatch::Callbacks', 'Schneiderlein::FlyCatcher'
+      end
     end
   end
 
